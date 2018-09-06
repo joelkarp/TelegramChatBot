7402a12aaf742679a5243c6f73e0f024a699636f
joelkarp <joel.karp@t-online.de>
2018-09-06 18:43:17 +0200

    2 Test cases hinzugefügt

---
+++ src/test/java/TelegramBot/TelegramChatBot/TelegramBotTests.java
@@ -0,0 +1,25 @@
+package TelegramBot.TelegramChatBot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import TelegramBot.TelegramChatBot.TelegramBot;
+
+public class TelegramBotTests {
+
+	TelegramBot teleBot = new TelegramBot();
+
+	@Test
+	public void getBotTokenTest() {
+		System.out.println("Test getBotTokenTest");
+		assertEquals("457679296:AAECBoRZxc9Co2dJOcJfZ0utwDa_rlB0ytY", teleBot.getBotToken());
+	}
+
+	@Test
+	public void getBotUsernameTest() {
+		System.out.println("Test getBotUsername");
+		assertEquals("MyFirstKinoBot", teleBot.getBotUsername());
+	}
+
+}
