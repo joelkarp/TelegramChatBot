3b7b995bffa1e468e4e9a91d318d4db05660d8f8
joelkarp <joel.karp@t-online.de>
2018-09-07 12:42:36 +0200

    test klasse leicht angepasst

--- src/test/java/TelegramBot/BotCommands/ActivCommandsTest.java
+++ src/test/java/TelegramBot/BotCommands/ActivCommandsTest.java
@@ -8,35 +8,32 @@ import org.junit.jupiter.api.Test;
 public class ActivCommandsTest {
 
 	ActivCommands commandList;
-	
+
 	@BeforeEach
 	public void beforEachTest() {
 		this.commandList = new ActivCommands();
 	}
-	
+
 	@Test
 	public void addCommandTest() {
 		assertEquals(0, commandList.getCommandCount());
 		commandList.addCommand("kinopolis programm", new KinopolisKinoProgram());
 		assertEquals(1, commandList.getCommandCount());
-		commandList.addCommand("kinopolis",new KinopolisKinoProgram());
+		commandList.addCommand("kinopolis", new KinopolisKinoProgram());
 		assertEquals(2, commandList.getCommandCount());
 	}
-	
+
 	@Test
 	public void removeCommandTest() {
 		assertEquals(0, commandList.getCommandCount());
 		commandList.addCommand("kinopolis programm", new KinopolisKinoProgram());
-		commandList.addCommand("kinopolis",new KinopolisKinoProgram());		
+		commandList.addCommand("kinopolis", new KinopolisKinoProgram());
 		assertEquals(2, commandList.getCommandCount());
-		
+
 		commandList.removeCommand("kinopolis programm");
 		assertEquals(1, commandList.getCommandCount());
 		commandList.removeCommand("kinopolis");
 		assertEquals(0, commandList.getCommandCount());
 	}
-	
-		
-	
-	
+
 }
