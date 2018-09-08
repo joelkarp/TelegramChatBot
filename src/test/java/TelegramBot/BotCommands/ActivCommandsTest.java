5b9611ab45ec69bbc02579b494dd00ecb1ec698a
joelkarp <joel.karp@t-online.de>
2018-09-08 13:23:43 +0200

    Packages repariert

--- src/test/java/TelegramBot/BotCommands/ActivCommandsTest.java
+++ src/test/java/TelegramBot/BotCommands/ActivCommandsTest.java
@@ -8,32 +8,34 @@ import org.junit.jupiter.api.Test;
 public class ActivCommandsTest {
 
 	ActivCommands commandList;
-
+	
 	@BeforeEach
 	public void beforEachTest() {
 		this.commandList = new ActivCommands();
 	}
-
+	
 	@Test
 	public void addCommandTest() {
 		assertEquals(0, commandList.getCommandCount());
 		commandList.addCommand("kinopolis programm", new KinopolisKinoProgram());
 		assertEquals(1, commandList.getCommandCount());
-		commandList.addCommand("kinopolis", new KinopolisKinoProgram());
+		commandList.addCommand("kinopolis",new KinopolisKinoProgram());
 		assertEquals(2, commandList.getCommandCount());
 	}
-
+	
 	@Test
 	public void removeCommandTest() {
 		assertEquals(0, commandList.getCommandCount());
 		commandList.addCommand("kinopolis programm", new KinopolisKinoProgram());
-		commandList.addCommand("kinopolis", new KinopolisKinoProgram());
+		commandList.addCommand("kinopolis",new KinopolisKinoProgram());		
 		assertEquals(2, commandList.getCommandCount());
-
 		commandList.removeCommand("kinopolis programm");
 		assertEquals(1, commandList.getCommandCount());
 		commandList.removeCommand("kinopolis");
 		assertEquals(0, commandList.getCommandCount());
 	}
-
+	
+		
+	
+	
 }
