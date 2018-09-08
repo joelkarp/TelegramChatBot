7e2cf00052bc99565eb3e1075371f8f47b3e767d
joelkarp <joel.karp@t-online.de>
2018-09-08 13:36:25 +0200

    Testcase verändert

--- src/test/java/TelegramBot/BotCommands/ActivCommandsTest.java
+++ src/test/java/TelegramBot/BotCommands/ActivCommandsTest.java
@@ -1,6 +1,7 @@
 package TelegramBot.BotCommands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,34 +9,44 @@ import org.junit.jupiter.api.Test;
 public class ActivCommandsTest {
 
 	ActivCommands commandList;
-	
+
 	@BeforeEach
 	public void beforEachTest() {
 		this.commandList = new ActivCommands();
 	}
-	
+
 	@Test
 	public void addCommandTest() {
 		assertEquals(0, commandList.getCommandCount());
 		commandList.addCommand("kinopolis programm", new KinopolisKinoProgram());
 		assertEquals(1, commandList.getCommandCount());
-		commandList.addCommand("kinopolis",new KinopolisKinoProgram());
+		commandList.addCommand("kinopolis", new KinopolisKinoProgram());
 		assertEquals(2, commandList.getCommandCount());
 	}
-	
+
 	@Test
 	public void removeCommandTest() {
 		assertEquals(0, commandList.getCommandCount());
 		commandList.addCommand("kinopolis programm", new KinopolisKinoProgram());
-		commandList.addCommand("kinopolis",new KinopolisKinoProgram());		
+		commandList.addCommand("kinopolis", new KinopolisKinoProgram());
 		assertEquals(2, commandList.getCommandCount());
 		commandList.removeCommand("kinopolis programm");
 		assertEquals(1, commandList.getCommandCount());
 		commandList.removeCommand("kinopolis");
 		assertEquals(0, commandList.getCommandCount());
 	}
-	
-		
-	
-	
+
+	@Test
+	public void getCommandTest() {
+		assertEquals(0, commandList.getCommandCount());
+		commandList.addCommand("kinopolis programm", new KinopolisKinoProgram());
+		commandList.addCommand("kinopolis", new KinopolisKinoProgram());
+		assertEquals(2, commandList.getCommandCount());
+
+		assertThrows(CommandNotFoundException.class, () -> {
+			commandList.getCommand("keinGueltigerKey");
+		});
+
+	}
+
 }
