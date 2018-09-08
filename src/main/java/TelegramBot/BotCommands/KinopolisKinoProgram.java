0d76c6977e2ad42cc40f8365595a61f985fe258f
joelkarp <joel.karp@t-online.de>
2018-09-08 17:20:02 +0200

    Kiopolis command logik hinzugefügt

--- src/main/java/TelegramBot/BotCommands/KinopolisKinoProgram.java
+++ src/main/java/TelegramBot/BotCommands/KinopolisKinoProgram.java
@@ -1,10 +1,45 @@
 package TelegramBot.BotCommands;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class KinopolisKinoProgram implements BotCommand{
 
 	public String execute() {
 		
-		return null;
-	}
+		Document doc;
+		try {
+			doc = Jsoup.connect("https://www.kinopolis.de/bn/programm/woche-1").get();
 
+			// System.out.println(links);
+			Elements el = doc.getElementsByClass("kc_box_in");
+
+			System.out.println(doc.title());
+
+			String ergebnis = doc.title();
+			
+			for (int i = 0; i < 8; i++) {
+				
+				ergebnis += "\n" + el.get(i).child(0).text();
+				ergebnis += "\n" + el.get(i).child(1).child(1).child(0).text();
+				ergebnis += "\n" + el.get(i).child(0).child(0).attr("abs:href");
+				ergebnis += "\n";
+				
+			}
+			// System.out.println(el.get(0));
+
+			return ergebnis;
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+			return "Fehler in der Abfrage";
+		}
+	}
 }
+
+
