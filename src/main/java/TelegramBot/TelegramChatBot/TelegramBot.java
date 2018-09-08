b2a92ee271bb5fad06e6fe1624042ca46afff07b
joelkarp <joel.karp@t-online.de>
2018-09-08 13:19:42 +0200

    struktur angepasst

--- src/main/java/TelegramBot/TelegramChatBot/TelegramBot.java
+++ src/main/java/TelegramBot/TelegramChatBot/TelegramBot.java
@@ -6,46 +6,44 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import TelegramBot.BotCommands.ActivCommands;
+import TelegramBot.BotCommands.CommandNotFoundException;
 import TelegramBot.BotCommands.KinopolisKinoProgram;
 
 public class TelegramBot extends TelegramLongPollingBot {
-	
+
 	private ActivCommands commandList;
-	
+
 	public TelegramBot() {
-		commandList =  new ActivCommands();
+		commandList = new ActivCommands();
 		initCommandList();
 	}
-	
+
 	private void initCommandList() {
 		commandList.addCommand("kinopolis", new KinopolisKinoProgram());
 	}
-	
+
 	public String getBotUsername() {
 
 		return "MyFirstKinoBot";
 
 	}
 
-	
 	public void onUpdateReceived(Update update) {
 
 		if (update.hasMessage() && update.getMessage().hasText()) {
-			
+
 			String message_text = update.getMessage().getText();
 			final long chat_id = update.getMessage().getChatId();
-			
+
 			String answer = getCommandAnswer(message_text);
 			sendMessage(answer, chat_id);
-			
-			
-			
+
 		} else {
-			
+
 			System.out.println("leere Nachicht erhalten");
-		
+
 		}
-		
+
 	}
 
 	@Override
@@ -54,27 +52,29 @@ public class TelegramBot extends TelegramLongPollingBot {
 		return "457679296:AAECBoRZxc9Co2dJOcJfZ0utwDa_rlB0ytY";
 
 	}
+
 	private String getCommandAnswer(String message_text) {
-		
-		return this.commandList.getCommand(message_text).execute();
-		
+		try {
+			return this.commandList.getCommand(message_text).execute();
+		}
+		catch(CommandNotFoundException e) {
+			return "Befehl leider nicht erkannt";
+		}
+				
+			
 	}
-	
-	private void sendMessage( String content , long chatID) {
-		
+
+	private void sendMessage(String content, long chatID) {
+
 		SendMessage newMessage = new SendMessage().setChatId(chatID);
 		newMessage.setText(content);
-		
+
 		try {
 			execute(newMessage);
-		}catch(TelegramApiException e) {
+		} catch (TelegramApiException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	
-	
 
 }
---
+++ src/test/java/TelegramBot/BotCommands/CommandNotFoundException.java
@@ -0,0 +1,10 @@
+package TelegramBot.BotCommands;
+
+public class CommandNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
